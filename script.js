12cf3b9b528acf45603028d8e2408c11b02f5510
LDantasA <leonardo.dantas0@gmail.com>
2021-12-09 18:45:02 -0300

    Adicionado o "pixel board" gerado pelo JavaScript

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 function generatePalette(colors) {
   for (let color of colors) {
-    let option = document.createElement('div');
-    option.className = 'color';
-    option.style.backgroundColor = color;
-    document.getElementById('color-palette').appendChild(option);
+    let element = document.createElement('div');
+    element.className = 'color';
+    element.style.backgroundColor = color;
+    document.getElementById('color-palette').appendChild(element);
+  }
+}
+
+function generateBoard(size) {
+  const pixelBoard = document.getElementById('pixel-board');
+  for (let line = 0; line < size; line++) {
+    pixelBoard.style.gridTemplateColumns += ' auto';
+    for (let pixel = 0; pixel < size; pixel++) {
+      let element = document.createElement('div');
+      element.className = 'pixel';
+      pixelBoard.appendChild(element);
+    }
   }
 }
 
 window.onload = function () {
   generatePalette(['black', 'red', 'green', 'blue']);
+  generateBoard(5);
 };
