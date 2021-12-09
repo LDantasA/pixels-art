8a2d24253ce93c6a8eb70aa1f0ceb838762122d1
LDantasA <leonardo.dantas0@gmail.com>
2021-12-09 19:34:25 -0300

    adicionado o sistema para selecionar as cores (ainda não há utilidade, mas prometo que está selecionando)

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function generatePalette(colors) {
+  const colorpalette = document.getElementById('color-palette');
+
   for (let color of colors) {
     let element = document.createElement('div');
     element.className = 'color';
     element.style.backgroundColor = color;
-    document.getElementById('color-palette').appendChild(element);
+    colorpalette.appendChild(element);
+    element.addEventListener('click', colorSelector);
   }
+
+  colorpalette.firstChild.classList.add('selected');
 }
 
 function generateBoard(size) {
   const pixelBoard = document.getElementById('pixel-board');
+
   for (let line = 0; line < size; line++) {
     pixelBoard.style.gridTemplateColumns += ' auto';
     for (let pixel = 0; pixel < size; pixel++) {
@@ -17,6 +23,16 @@ function generateBoard(size) {
       pixelBoard.appendChild(element);
     }
   }
+}
+
+function colorSelector(event) {
+  let selectedColor = document.getElementsByClassName('selected')[0];
+
+  if (selectedColor != undefined) {
+    selectedColor.classList.remove('selected');
+  }
+
+  event.target.classList.add('selected');
 }
 
 window.onload = function () {
